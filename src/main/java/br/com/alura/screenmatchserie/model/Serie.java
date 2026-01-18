1854d0dca33c7600feb8e35b34d10bba59634961
Helber Farias <helberfarias.ptpro@gmail.com>
2026-01-18 20:41:36 -0300

    criação do package repositoy

--- src/main/java/br/com/alura/screenmatchserie/model/Serie.java
+++ src/main/java/br/com/alura/screenmatchserie/model/Serie.java
@@ -1,23 +1,29 @@
 package br.com.alura.screenmatchserie.model;
 
 import br.com.alura.screenmatchserie.service.ConsultaMyMemory;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.OptionalDouble;
 
 @Entity
 @Table(name = "series")
 public class Serie {
-    
-    private  String titulo;
-    private  Integer totalTemporada;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true) //não permite repetição do titulo da serie
+    private String titulo;
+    private Integer totalTemporada;
     private Double avaliacao;
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
     private String atores;
     private String poster;
     private String sinopse;
+    @Transient
+    private List<Episodio> episodios = new ArrayList<>();
 
     public Serie (DadosSerie dadosSerie) {
         this.titulo = dadosSerie.titulo();
@@ -29,6 +35,22 @@ public class Serie {
         this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse());
 
 
+    }
+
+    public List<Episodio> getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(List<Episodio> episodios) {
+        this.episodios = episodios;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitulo() {
