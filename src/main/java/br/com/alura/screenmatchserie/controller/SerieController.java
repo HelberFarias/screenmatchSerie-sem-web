bcce27233d76c21ba09564a6b8abfc98519d0e14
Helber Farias <helberfarias.ptpro@gmail.com>
2026-01-27 21:20:57 -0300

    criacao package DTO e record SerieDTO

--- src/main/java/br/com/alura/screenmatchserie/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatchserie/controller/SerieController.java
@@ -1,16 +1,28 @@
 package br.com.alura.screenmatchserie.controller;
 
+import br.com.alura.screenmatchserie.dto.SerieDTO;
 import br.com.alura.screenmatchserie.model.Serie;
+import br.com.alura.screenmatchserie.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
-    @GetMapping ("/series")
-    public List<Serie> obterSeries() {
+    @Autowired
+    private SerieRepository repositorio;
 
-    }
+
+    @GetMapping ("/series")
+    public List<SerieDTO> obterSeries() {
+        return repositorio.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporada(), s.getAvaliacao()
+                        , s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+     }
 }
---
+++ src/main/java/br/com/alura/screenmatchserie/dto/SerieDTO.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatchserie.dto;
+
+import br.com.alura.screenmatchserie.model.Categoria;
+
+public record SerieDTO(Long id,
+                       String titulo,
+                       Integer totalTemporada,
+                       Double avaliacao,
+                       Categoria genero,
+                       String atores,
+                       String poster,
+                       String sinopse) {
+}
